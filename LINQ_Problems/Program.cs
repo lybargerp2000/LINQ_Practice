01b7286e4f6862eb813bb2005bc9ab67007c3174
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-03-09 15:08:04 -0500

    Finished second problem. Working on #3.

--- LINQ_Problems/Program.cs
+++ LINQ_Problems/Program.cs
@@ -11,8 +11,6 @@ namespace LINQ_Problems
         static void Main(string[] args)
         {
             List<string> words = new List<string> { "the", "bike", "this", "it", "tenth", "mathematics" };
-            //WordLibrary wordLibrary = new WordLibrary();
-            //List<Word> words = wordLibrary.GetWords();
             List<string> wordsWithTH = words.Where(s => s.Contains("th")).ToList();
             foreach (var word in wordsWithTH)
             {
@@ -20,6 +18,22 @@ namespace LINQ_Problems
               
             }
             Console.ReadLine();
+
+
+            List<string> names = new List<string> { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            IEnumerable<string> result = names.Distinct();
+            foreach(var sn in result)
+            {
+                Console.WriteLine(sn);
+            }
+            Console.ReadLine();
+
+            List<string> classGrades = new List<string>() {"80,100,92,89,65",
+             "93,81,78,84,69",
+             "73,88,83,99,64",
+             "98,100,66,74,55"};
+            var result = 
+
         }
     }
 }
