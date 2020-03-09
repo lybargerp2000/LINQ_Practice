f5984e5d6fc66ef184e44337663f070a6785ef40
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-03-09 11:02:32 -0500

    Creating classes and logic for first problem.

---
+++ LINQ_Problems/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+        }
+    }
+}
---
+++ LINQ_Problems/WordLibrary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Problems
+{
+    class WordLibrary
+    {
+     public List<Word> GetWords()
+        {
+            return new List<Word>()
+            {
+                new Word() {word = "the"},
+                new Word() {word = "bike"},
+                new Word() {word = "this"},
+                new Word() {word = "it"},
+                new Word() {word = "tenth"},
+                new Word() {word = "mathemmatics"}
+
+            };
+        }
+
+
+
+    }
+}
